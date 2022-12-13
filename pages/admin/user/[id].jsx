31485c7ab18623751cd1d0b1d7549dc026529f86
Stephen <stvlley.amzn@gmail.com>
2022-12-13 09:21:09 -0500

    x

---
+++ pages/admin/user/[id].jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../../../components/Layout';
+
+export default function EditUserScreen(){
+    return (
+        <Layout title='Edit Users'>
+            <h1>Edit Users</h1>
+        </Layout>
+
+    );
+}
+
+
